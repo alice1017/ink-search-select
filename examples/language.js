1947582373e30a7616d88fd3894e2b1a79435b55
Alice1017 <genocidedragon@gmail.com>
2019-06-03 15:14:14 +0900

    MODIFIED: src/index.js -> examples/language.js

---
+++ examples/language.js
@@ -0,0 +1,27 @@
+import SearchSelect from "./component";
+import React from 'react';
+import { render } from 'ink';
+
+let app;
+const languages = [
+    "JavaScript", "Java", "Python", "Ruby",
+    "TypeScript", "ECMAScript", "AppleScript",
+    "Swift", "Perl", "Lua", "Clojure", "C++", "C",
+    "Go", "Haskell", "Scala", "Scheme", "BrainFu*k"
+];
+const items = languages.map(lang => {
+    return { label: lang, value: lang.toLowerCase() };
+});
+const onSelect = (item) => {
+    console.log(`You selected: '${item.label}'`);
+    app.unmount();
+};
+
+app = render(
+    <SearchSelect
+        items={items}
+        label="Query: "
+        desc="Search any languages"
+        onSelect={onSelect}
+    />
+);
